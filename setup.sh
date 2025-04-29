a12d9b6d748b7b4e05a47599d128da11b5eb72e8
Rodion Kachkin <kyleRQWS@gmail.com>
2025-04-29 23:25:08 +0400

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd)":/var/www/html \
+    -w /var/www/html laravelsail/php84-composer:latest \
+    bash -c "
+      composer create-project laravel/laravel;
+      cd laravel && mv ./* ../ && mv ./.* ../ && cd - && rm -rf laravel;
+
+      composer require laravel/sail --dev && composer install;
+      php artisan sail:install --with=mariadb;
+
+      composer require laravel/breeze;
+      php artisan breeze:install;
+      php artisan migrate;
+    "
