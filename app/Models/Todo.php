967c46dc70c9d821a0f5208fb9fd780606ed3ae4
Rodion Kachkin <kyleRQWS@gmail.com>
2025-04-30 12:41:36 +0400

    Update Todo creation migrations and link user and Todo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Todo extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2025_04_30_055931_create_todos_table.php
+++ database/migrations/2025_04_30_055931_create_todos_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('name');
             $table->timestamps();
         });
     }
