e6a8bdbc2398c6242cdf0cb6771c2be3f1b54196
Rodion Kachkin <kyleRQWS@gmail.com>
2025-05-02 20:14:18 +0400

    Fixes Todo List component class

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -3,39 +3,38 @@
 namespace App\Livewire;
 
 use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\On;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 
 class TodoList extends Component
 {
-    public Collection $todos;
-
     public function render(): object
     {
-        return view('livewire.todo-list');
-    }
-
-    public function mount(): void
-    {
-        $this->todos = Auth::user()->todos()->get();
+        return view('livewire.todo-list', [
+            'todos' => $this->todos(),
+        ]);
     }
 
     #[On('todo-created')]
     public function addTodo(int $id): void
     {
-        $todo = Auth::user()->todos()->find($id);
+        $todo = $this->todos()->find($id);
         $this->authorize('view', $todo);
-
-        $this->todos->prepend($todo);
     }
 
     public function deleteTodo(int $id): void
     {
-        $todo = Auth::user()->todos()->find($id);
+        $todo = $this->todos()->find($id);
         $this->authorize('delete', $todo);
 
         $todo->delete();
-        $this->todos = $this->todos->filter(fn($t) => $t->id !== $id);
+    }
+
+    #[Computed]
+    private function todos(): Collection
+    {
+        return Auth::user()->todos()->orderByDesc('created_at')->get();
     }
 }
