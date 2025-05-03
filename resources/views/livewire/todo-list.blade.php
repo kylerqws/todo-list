7c4abf7a8fae744111c7d4b24e9883d6867473df
Rodion Kachkin <kyleRQWS@gmail.com>
2025-05-03 16:08:04 +0400

    Fix style for Todo status checkbox to SVG images

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -2,8 +2,20 @@
     @isset($todos)
         @foreach($todos as $todo)
             <div wire:key='{{ $todo->id }}' class="flex items-center h-14">
-                <input type="checkbox" wire:click="toggleStatus({{ $todo->id }})" @checked($todo->status) />
-                <div class="w-full">{{ $todo->name }}</div>
+                <button type="button" wire:click="toggleStatus({{ $todo->id }})"
+                        class="w-6 h-6 relative focus:outline-none">
+                    @if ($todo->status)
+                        <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none">
+                            <circle cx="12" cy="12" r="10" stroke="#f97316" stroke-width="2" />
+                            <circle cx="12" cy="12" r="6" fill="#f97316" />
+                        </svg>
+                    @else
+                        <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none">
+                            <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2" />
+                        </svg>
+                    @endif
+                </button>
+                <div class="w-full ml-3">{{ $todo->name }}</div>
                 <div class="px-2 ml-3">
                     <x-danger-button wire:click="deleteTodo({{ $todo->id }})" wire:loading.attr="disabled">
                         {{ __('Delete') }}
